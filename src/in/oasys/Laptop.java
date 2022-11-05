adfb56decab59a2e8586bf57756c8d1e7a845f92
Mathumitha-19 <mathu1461@gmail.com>
2022-11-05 13:11:26 +0530

    Project New1

--- src/in/oasys/Laptop.java
+++ src/in/oasys/Laptop.java
@@ -4,6 +4,7 @@ public class Laptop {
 String bodyColour;
 int screenSize;
 float price;
+String brand;
 }
 
 
--- src/in/oasys/UseLaptop.java
+++ src/in/oasys/UseLaptop.java
@@ -6,7 +6,8 @@ public static void main (String args[]) {
 	l1.bodyColour="black";
 	l1.screenSize=7;
 	l1.price=10500.50f;
-	System.out.println(l1.bodyColour+" "+l1.screenSize+" "+l1.price);
+	l1.brand="Hp";
+	System.out.println(l1.bodyColour+" "+l1.screenSize+" "+l1.price+" "+l1.brand);
 	
 	
 	
